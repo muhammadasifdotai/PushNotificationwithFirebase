8447b7a54b554afc51204f13c9f2ac40b33ec509
muhammadasifdotai <145665646+muhammadasifdotai@users.noreply.github.com>
2025-02-23 22:15:28 +0500

    token logic added

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export default function App(): JSX.Element {
+
+  useEffect(() => {
+    getDeviceToken()
+  }, [])
+
+  const getDeviceToken = async () => {
+    const token = await messaging().getToken()
+    console.log(token)
+  }
 
   return (
     <View style={styles.main}>
